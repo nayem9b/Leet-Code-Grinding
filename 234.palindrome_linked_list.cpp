5b71c44ad2fce2bd2c9a271098202aef41f89215
Md Razwan Niam <razwanniam1@gmail.com>
2024-02-15 00:27:32 +0600

    defanging ip-1108

--- 234.palindrome_linked_list.cpp
+++ 234.palindrome_linked_list.cpp
@@ -1,41 +1,41 @@
 class PalindromeLinkedList {
+public:
+    bool isPalindrome(ListNode* head) {
+        // Find the middle
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        if (fast != nullptr) { // odd nodes: let right half smaller
+            slow = slow->next;
+        }
 
-  boolean isPalindrome(ListNode head) {
+        // Reverse the second half
+        slow = reverseList(slow);
+        fast = head;
 
-    // Find the middle
-    ListNode fast = head, slow = head;
-    while (fast != null && fast.next != null) {
-      fast = fast.next.next;
-      slow = slow.next;
+        // Start comparing one by one
+        while (slow != nullptr) {
+            if (fast->val != slow->val)
+                return false;
+
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return true;
     }
-    if (fast != null) { // odd nodes: let right half smaller
-      slow = slow.next;
+
+private:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        while (head != nullptr) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
-
-    // Reverse the second half
-    slow = reverseList(slow);
-    fast = head;
-
-    // Start comparing one by one
-    while (slow != null) {
-      if (fast.val != slow.val)
-        return false;
-
-      fast = fast.next;
-      slow = slow.next;
-    }
-    return true;
-  }
-
-  private ListNode reverseList(ListNode head) {
-    ListNode prev = null;
-    while (head != null) {
-      ListNode next = head.next;
-      head.next = prev;
-      prev = head;
-      head = next;
-    }
-    return prev;
-  }
-
-}
+};
