0a6a99a548675e0a2010105b422db73dfdf23fe4
Md Razwan Niam <razwanniam1@gmail.com>
2024-03-04 19:55:09 +0600

    leetcode 3

---
+++ 3.Longest_substring_without_repeating_chars.py
@@ -0,0 +1,13 @@
+class solution:
+    def lengthOfLongestSubstring(self, s:str)->:
+        charSet =set()
+        l=0
+        res=0
+
+        for r in range(len(s)):
+          while s[r] in charSet:
+            charSet.remove(s[l])
+            l+=1
+          charSet.add(s[r])
+          res=max(res,r-l+1)
+        return res
---
+++ 3.longest_substring.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <unordered_set>
+#include <algorithm>
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(std::string s) {
+        std::unordered_set<char> charSet;
+        int l = 0;
+        int res = 0;
+
+        for (int r = 0; r < s.length(); ++r) {
+            while (charSet.find(s[r]) != charSet.end()) {
+                charSet.erase(s[l]);
+                ++l;
+            }
+            charSet.insert(s[r]);
+            res = std::max(res, r - l + 1);
+        }
+        return res;
+    }
+};
