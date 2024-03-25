f70c1409a94a6d85c9fc3dcffeab5c9c28e90c31
nayem9b <razwanniam1@gmail.com>
2024-03-26 01:41:07 +0600

    find all duplicate in array:

--- 442.find_all_duplicate_in_array.cpp
+++ 442.find_all_duplicate_in_array.cpp
@@ -1,5 +1,3 @@
-// USING UNORDERED MAP
-
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
@@ -10,5 +8,26 @@ public:
             else mp[i]++;
         }
         return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> resultSet;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            // Get the index the element corresponds to
+            int index = abs(nums[i]) - 1;
+
+            // If the number is already negative, it means we are encountering
+            // it twice
+            if (nums[index] < 0)
+                resultSet.push_back(index + 1);
+            // Flip the number at the index to negative
+            nums[index] *= -1;
+        }
+
+        return resultSet;
     }
 };
