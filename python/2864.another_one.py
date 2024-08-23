5cf49befe6dbf1a4f3f27dd2d8a9a73c9d214470
nayem9b <razwanniam1@gmail.com>
2024-08-24 02:53:02 +0600

    sync cpp

--- python/2864.another_one.py
+++ python/2864.another_one.py
@@ -1,11 +1,18 @@
-class Solution:
-    def maximumOddBinaryNumber(self, s: str) -> str:
-        s = [c for c in s]
-        left = 0
-        for i in range(len(s)):
-            if s[i] == "1":
-                s[i], s[left] = s[left], s[i]
-                left += 1
+# class Solution:
+#     def maximumOddBinaryNumber(self, s: str) -> str:
+#         s = [c for c in s]
+#         left = 0
+#         for i in range(len(s)):
+#             if s[i] == "1":
+#                 s[i], s[left] = s[left], s[i]
+#                 left += 1
 
-        s[left - 1], s[len(s) - 1] = s[len(s) - 1], s[left - 1]
-        return "".join(s)
+#         s[left - 1], s[len(s) - 1] = s[len(s) - 1], s[left - 1]
+#         return "".join(s)
+
+
+# Taking an integer input from the user
+t = int(input())
+
+# Outputting the integer
+print(t)
