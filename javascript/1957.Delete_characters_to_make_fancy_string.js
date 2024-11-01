351efb246d562c85713755d830834aa3d2432760
Md Razwan Niam <78756747+nayem9b@users.noreply.github.com>
2024-11-02 04:52:58 +0600

    leetcode 1957

---
+++ javascript/1957.Delete_characters_to_make_fancy_string.js
@@ -0,0 +1,20 @@
+
+const makeFancyString = (s) =>{
+    let res=''
+    let ls='.'
+    let count=0
+    for(let i=0;i<s.length;i++){
+        if(s[i]==ls)
+            count++
+        else {
+            count=1
+            ls=s[i]
+        }
+
+        if(count>2){
+            continue
+        }
+        res+=s[i]
+    }
+    return res
+};
